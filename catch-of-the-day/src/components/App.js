023773ac50a9fc06239d6fd0a91d7ff0c85b1f17
Leo <leo.pan.duh@gmail.com>
2019-05-23 15:17:51 -0400

    complete lesson 14

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,11 +2,16 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {}
+  };
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
   };
 
   addFish = fish => {
@@ -25,7 +30,10 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
